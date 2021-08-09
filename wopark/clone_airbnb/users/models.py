8460598b33174dbddc3a1e01879cf60836ad18c1
WONILLISM <wonillism@gmail.com>
2021-08-09 15:27:56 +0900

    add user model

--- wopark/clone_airbnb/users/models.py
+++ wopark/clone_airbnb/users/models.py
@@ -1,5 +1,40 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(models.Model):
 
-    pass
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(null=True, blank=True, choices=GENDER_CHOICES, max_length=10)
+    bio = models.TextField(null=True, blank=True, default="")
+    birthday = models.DateField(null=True, blank=True)
+    language = models.CharField(null=True, blank=True, choices=LANGUAGE_CHOICES, max_length=2)
+    currency = models.CharField(null=True, blank=True, choices=CURRENCY_CHOICES, max_length=3)
+    superhost = models.BooleanField(default=False)
