de3d40006dd3aa048780e1e76efa6538b0834c81
WONILLISM <wonillism@gmail.com>
2021-08-10 17:17:05 +0900

    migrations

--- wopark/clone_airbnb/reservations/migrations/0001_initial.py
+++ wopark/clone_airbnb/reservations/migrations/0001_initial.py
@@ -12,12 +12,14 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='List',
+            name='Reservation',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('updated', models.DateTimeField(auto_now=True)),
-                ('name', models.CharField(max_length=80)),
+                ('check_in', models.DateField()),
+                ('check_out', models.DateField()),
+                ('status', models.CharField(choices=[('pending', 'pending'), ('confirmed', 'confirmed'), ('canceled', 'canceled')], default='pending', max_length=12)),
             ],
             options={
                 'abstract': False,
