7a2b3ac2d304cee163e81ad9009cad1408a2a152
WONILLISM <wonillism@gmail.com>
2021-08-16 16:15:08 +0900

    add custom command

---
+++ wopark/clone_airbnb/rooms/management/commands/seed_amenities.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from rooms.models import Amenity
+
+
+class Command(BaseCommand):
+
+    help = "This command creates amenities"
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument(
+    #         "--times",
+    #         help="How many times do you want me to tell you that I love you?",
+    #     )
+
+    def handle(self, *args, **options):
+
+        amenities = [
+            "Kitchen",
+            "Heating",
+            "Air conditioning",
+            "Washer",
+            "Dryer",
+            "Wifi",
+            "Breakfast",
+            "Indoor fireplace",
+            "Iron",
+            "Hair dryer",
+            "Dedicated workspace",
+            "TV",
+            "Crib",
+            "High chair",
+            "Self check-in",
+            "Smoke alarm",
+            "Carbon monoxide alarm",
+            "Private bathroom",
+            "Ski-in/ski-out",
+            "Beachfront",
+            "Waterfront",
+        ]
+        for amenity in amenities:
+            Amenity.objects.create(name=amenity)
+        self.stdout.write(self.style.SUCCESS("Amenities created!"))
