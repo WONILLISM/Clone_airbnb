831170c0ffcd7e8015eef3a805df48d6d772147f
mki <mki@student.42seoul.kr>
2021-08-27 14:11:12 +0900

    ch15.0

--- mki/users/views.py
+++ mki/users/views.py
@@ -25,3 +25,14 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "mj",
+        "last_name": "Ki",
+        "email": "mki@student.42seoul.kr",
+    }
